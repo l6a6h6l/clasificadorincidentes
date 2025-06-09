178f9bfa0886cbf111762752c07097f5c4aebe61
l6a6h6l <64456111+l6a6h6l@users.noreply.github.com>
2025-06-09 07:27:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import './App.css';
+import IncidentServicePredictor from './components/IncidentServicePredictor';
 
+function App() {
+  return (
+    <div className="App">
+      <IncidentServicePredictor />
+    </div>
+  );
+}
+
+export default App;
